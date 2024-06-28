0e5f1e17c04b40dc319e41a22ec4112bbf083dd6
andynze1 <andynze4@gmail.com>
2024-06-28 14:25:02 -0500

    adding Jenkins

--- Jenkins-SonarQube-VM/main.tf
+++ Jenkins-SonarQube-VM/main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "web" {
-  ami = "ami-0e001c9271cf7f3b9"
-  instance_type = "t2.large"
-  key_name = "aws-key1"
-  vpc_security_group_ids = [ aws_security_group.Jenkins-VM-SG.id ]
-  user_data = templatefile("./install.sh", {})
+  ami                    = "ami-0e001c9271cf7f3b9"
+  instance_type          = "t2.large"
+  key_name               = "aws-key1"
+  vpc_security_group_ids = [aws_security_group.Jenkins-VM-SG.id]
+  user_data              = templatefile("./install.sh", {})
 
   tags = {
     Name = "Jenkins-SonarQube"
@@ -14,28 +14,28 @@ resource "aws_instance" "web" {
 }
 
 resource "aws_security_group" "Jenkins-VM-SG" {
-  name = "Jenkins-VM-SG"
+  name        = "Jenkins-VM-SG"
   description = "Allow TLS inbound traffic"
 
   ingress = [
-    for port in [22, 80, 443, 8080, 9000, 3000] : {
-        description = "Inbound rules"
-        from_port   = port
-        to_port     = port
-        protocol    = "tcp"
-        cidr_blocks  = ["0.0.0.0/0"]
-        ipv6_cidr_block = []
-        prefix_list_ids = []
-        security_groups = []
-        self            = false
+    for port in [22, 80, 443, 8080, 9000, 3000, 8080] : {
+      description      = "inbound rules"
+      from_port        = port
+      to_port          = port
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
     }
   ]
 
-  egress = {
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks  = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
     Name = "Jenkins-VM-SG"
