43b78a10623522cdb01c3f70ebc623b9d55632c1
andynze1 <andynze4@gmail.com>
2024-07-03 11:37:48 -0500

    update to userdata

--- Jenkins-SonarQube-VM/install.sh
+++ Jenkins-SonarQube-VM/install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-sudo hostnamectl set-hostname Jenkins-SonarQube
+sudo hostnamectl set-hostname jenkins-sonarqube
+
+sudo apt update -y
+sudo apt install wget curl temurin-17-jdk -y
 
 # Install Jenkins
-sudo apt update -y
 wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc
 echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
 sudo apt update -y
-sudo apt install temurin-17-jdk -y
 /usr/bin/java --version
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
