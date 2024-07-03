0ed3fe35cf0967eeef6ed452c6101e6a07dc55ea
andynze1 <andynze4@gmail.com>
2024-07-03 12:02:20 -0500

    installing kubectl eksctl and helm

--- Jenkins-SonarQube-VM/install.sh
+++ Jenkins-SonarQube-VM/install.sh
@@ -40,7 +40,23 @@ echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.
 sudo apt-get update
 sudo apt-get install trivy -y
 
+# Install Kubectl
+sudo apt update
+curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
 
+# Install  eksctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+cd /tmp
+sudo mv /tmp/eksctl /bin
+eksctl version
+
+# Install helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+sudo chmod 700 get_helm.sh
+sudo ./get_helm.sh
+helm version
 
 
 
